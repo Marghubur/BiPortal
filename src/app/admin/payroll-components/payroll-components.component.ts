eff40a22ff110942e3717834a0e4f29daa5b7884
Marghub <marghub12@gmail.com>
2022-06-20 07:44:22 +0530

    added excel sheet upload method in component create

--- src/app/admin/payroll-components/payroll-components.component.ts
+++ src/app/admin/payroll-components/payroll-components.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ResponseModel } from 'src/auth/jwtService';
-import { AjaxService } from 'src/providers/ajax.service';
-import { ErrorToast, Toast } from 'src/providers/common-service/common.service';
+import { AjaxService, tableConfig } from 'src/providers/ajax.service';
+import { ErrorToast, Toast, WarningToast } from 'src/providers/common-service/common.service';
 import { Settings } from 'src/providers/constants';
+import { Dictionary } from 'src/providers/Generic/Code/Dictionary';
 import { iNavigation } from 'src/providers/iNavigation';
+import { Filter } from 'src/providers/userService';
+import { read, utils } from 'xlsx';
 declare var $: any;
 
 @Component({
@@ -31,6 +34,21 @@ export class PayrollComponentsComponent implements OnInit {
   AdhocBonus: Array<PayrollComponentsModal> = [];
   movableComponent: Array<any> = [];
   isReady = false;
+  isUploadFile: boolean = true;
+  fileSize: string;
+  fileName: string;
+  isFileReady: boolean = false;
+  noOfRecords: number;
+  recordToUpload: any;
+  ws: any;
+  file: File;
+  tableConfiguration: tableConfig = null;
+  isAvailable: boolean = false;
+  uploadedCandidatesData: Filter = null;
+  isDisable: boolean = true;
+  SalaryComponentsDetail: Array<any> = [];
+  expandedTable: boolean = true;
+
 
   constructor(private fb: FormBuilder,
               private http: AjaxService,
@@ -336,6 +354,166 @@ export class PayrollComponentsComponent implements OnInit {
     e.target.value = '';
     this.RecurringComponent = this.AllComponents.filter (x => x.IsAdHoc == false);
     this.isReady = true;
+  }
+
+  excelSheetModal() {
+    $('#excelSheetModal').modal('show');
+  }
+
+  readExcelData(e: any) {
+    this.file = e.target.files[0];
+    if (this.file !== undefined && this.file !== null) {
+      this.convertToJson(false).then(data => {
+        if (data) {
+          this.recordToUpload = data;
+          this.fileSize = (this.file.size / 1024).toFixed(2);
+          this.fileName = this.file.name;
+          this.noOfRecords = this.recordToUpload.length;
+          this.isFileReady = true;
+          this.isDisable = false;
+          this.isUploadFile = false;
+          let excelData = data.mapTable[0];
+          let rows: any = excelData;
+          if (excelData) {
+            this.tableConfiguration = new tableConfig();
+            this.tableConfiguration.totalRecords = 1;
+            this.tableConfiguration.header = excelData.value.Keys;
+            this.tableConfiguration.data = rows.value.Data;
+            // this.uploadedCandidatesData.TotalRecords = 0;
+            // if(this.tableConfiguration.data.length > 0) {
+            //   this.uploadedCandidatesData.TotalRecords = this.tableConfiguration.data.length;
+            // }
+            this.SalaryComponentsDetail = this.tableConfiguration.data;
+            this.tableConfiguration.isEnableAction = true;
+            this.isAvailable = true;
+          }
+        } else {
+          this.cleanFileHandler();
+          ErrorToast("Excel data is not valid.");
+        }
+      });
+    }
+  }
+
+  convertToJson(onlyHeader: boolean = true): Promise<any> {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+      let workbookkk;
+      let XL_row_object;
+      let TempDictionary = new Dictionary<string, any>();
+      reader.readAsBinaryString(this.file);
+      reader.onload = function () {
+        let data = reader.result;
+        workbookkk = read(data, { type: "binary" });
+        workbookkk.SheetNames.forEach(function (sheetName) {
+          XL_row_object = utils.sheet_to_json(workbookkk.Sheets[sheetName]);
+          let position = TempDictionary.hasKey(sheetName);
+          if (
+            position === -1 &&
+            XL_row_object !== null &&
+            XL_row_object.length > 0
+          ) {
+            let RowDetail = XL_row_object[0];
+            let ColumnDetail = [];
+            if (RowDetail !== null) {
+              if (typeof RowDetail === "object") {
+                let Keys = Object.keys(RowDetail);
+                let index = 0;
+                let Type = "";
+                while (index < Keys.length) {
+                  Type = typeof RowDetail[Keys[index]];
+                  if (
+                    Type === "undefined" ||
+                    RowDetail[Keys[index]] === null ||
+                    RowDetail[Keys[index]] == ""
+                  ) {
+                    Type = "string";
+                  }
+                  ColumnDetail.push({
+                    ColumnName: Keys[index],
+                    ColumnType: Type
+                  });
+                  index++;
+                }
+              }
+            }
+            let SheetData = {
+              Keys: ColumnDetail,
+              Data: onlyHeader ? null : XL_row_object
+            };
+            TempDictionary.insert(sheetName, SheetData);
+          }
+          resolve(TempDictionary);
+        });
+      };
+    });
+  }
+
+  expandTable() {
+    this.expandedTable = false;
+  }
+  closeExpandModel(){
+    this.expandedTable = true;
+  }
+
+  excelfireBrowserFile() {
+    $("#uploadexcel").click();
+  }
+
+  cleanFileHandler() {
+    $("#uploadexcel").val("");
+    this.fileSize = "";
+    this.fileName = "";
+    this.isFileReady = false;
+    this.noOfRecords = 0;
+    event.stopPropagation();
+    event.preventDefault();
+    this.isAvailable=false;
+    this.isDisable = true;
+    this.isUploadFile = true;
+  }
+
+  uploadExcelSheet($e: any) {
+    this.isLoading = true;
+    $e.preventDefault();
+    $e.stopPropagation();
+    let errroCounter = 0;
+    let i = 0;
+    while (i < this.SalaryComponentsDetail.length) {
+      let componentId = this.SalaryComponentsDetail.filter(x => x.ComponentId == this.SalaryComponentsDetail[i].ComponentId);
+      let componentname = this.SalaryComponentsDetail.filter(x => x.ComponentFullName == this.SalaryComponentsDetail[i].ComponentFullName);
+      if (componentId.length > 1 || componentname.length > 1) {
+        ErrorToast("Component Name or Component Full Name are duplicate.");
+        errroCounter ++;
+      }
+      i++;
+    }
+
+    if (errroCounter === 0 && this.SalaryComponentsDetail.length > 0) {
+      this.http.post("SalaryComponent/InsertUpdateSalaryComponentsByExcel", this.SalaryComponentsDetail)
+      .then((response: ResponseModel) => {
+        if (response.ResponseBody) {
+          let data = response.ResponseBody;
+          if (data.length > 0) {
+            this.RecurringComponent = data;
+            this.AllComponents = data;
+            let i =0;
+            while(i < this.RecurringComponent.length) {
+              this.componentType(this.RecurringComponent[i].ComponentTypeId, i);
+              i++;
+            }
+            this.cleanFileHandler();
+            $('#excelSheetModal').modal('hide');
+            Toast("Data Uploaded successfull");
+            this.isLoading = false;
+          }
+        } else {
+          ErrorToast("Unable to upload the data");
+        }
+      });
+    } else {
+      WarningToast("Please upload atleast one record");
+    }
   }
 }
 
