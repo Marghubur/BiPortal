d71f30179d1763f15a30341065b59d4ba0c810fd
bottomhalf <bottomhalf.dev@gmail.com>
2022-06-25 07:56:09 +0530

    name changed

--- src/app/admin/declaration/declaration.component.ts
+++ src/app/admin/declaration/declaration.component.ts
@@ -34,10 +34,11 @@ export class DeclarationComponent implements OnInit, AfterViewChecked {
   taxSavingAllowance: Array<IncomeDeclaration> = [];
   cachedData: any = null;
   taxAmount: TaxAmount = new TaxAmount();
+  myDeclaration: Array<MyDeclaration> = [];
   year: number = 0;
   taxCalender: Array<any> = [];
   monthlyTaxAmount: MonthlyTax;
-  employeeDeclaration: any = {};
+  allComponentDetails: any = {};
   exemptionComponent: Array<any> = [];
   filterValue: string = '';
   editException: boolean = false;
@@ -57,8 +58,6 @@ export class DeclarationComponent implements OnInit, AfterViewChecked {
   isEmployeesReady: boolean = false;
   applicationData: any = [];
   employeesList: autoCompleteModal = new autoCompleteModal();
-  currentEmployee: any = null;
-  userName: string = "";
 
   constructor(private local: ApplicationStorage,
     private user: UserService,
@@ -70,7 +69,6 @@ export class DeclarationComponent implements OnInit, AfterViewChecked {
     this.filterValue = '';
     this.rentalPage =1;
     this.rentedResidence();
-    this.EmployeeId = 4;
     this.loadData();
     var dt = new Date();
     var month = 3;
@@ -105,52 +103,53 @@ export class DeclarationComponent implements OnInit, AfterViewChecked {
       i++;
     }
 
-    this.getDeclaration(this.EmployeeId);
-  }
-
-  ngAfterViewChecked(): void {
-    $('[data-bs-toggle = "tooltip"]').tooltip();
-  }
-
-  filterDeduction() {
-    let value = this.filterValue.toLocaleUpperCase();
-  }
-
-  resetFilter() {
-    this.filterValue = '';
-
-    this.ExemptionDeclaration = this.employeeDeclaration.ExemptionDeclaration;
-    this.OtherDeclaration = this.employeeDeclaration.OtherDeclaration;
-    this.TaxSavingAlloance = this.employeeDeclaration.TaxSavingAlloance;
-  }
-
-  getDeclaration(id: any) {
-    this.EmployeeId = id;
-    this.SectionIsReady = false;
-    this.http.get(`Declaration/GetEmployeeDeclarationDetailById/${this.EmployeeId}`).then((response:ResponseModel) => {
-      if (response.ResponseBody) {
-        if(response.ResponseBody.SalaryComponentItems && response.ResponseBody.SalaryComponentItems.length > 0) {
-          this.employeeDeclaration = response.ResponseBody;
-          this.resetFilter();
-          this.EmployeeDeclarationId = response.ResponseBody.EmployeeDeclarationId;
-          this.employeeEmail = response.ResponseBody.Email;
-        }
-
-        if (response.ResponseBody && response.ResponseBody.FileDetails)
-          this.declarationFiles = response.ResponseBody.FileDetails;
-
-        Toast("Declaration detail loaded successfully");
-      }
-
-      this.SectionIsReady = true;
-    })
-
     this.taxAmount = {
       NetTaxableAmount: 2050000,
       TotalTaxPayable: 444600,
       TaxAlreadyPaid: 37050,
       RemainingTaxAMount: 444600 - 37050
     };
+    this.myDeclaration.push({
+      Declaration: "1.5 Lac Exemptions",
+      NoOfDeclaration: 2,
+      AmountDeclared: 21600,
+      ProofSUbmitted: 0,
+      AmountRejected: 0,
+      AmountAccepted: 0
+    },
+    {
+      Declaration: "Other Exemptions",
+      NoOfDeclaration: 0,
+      AmountDeclared: 0,
+      ProofSUbmitted: 0,
+      AmountRejected: 0,
+      AmountAccepted: 0
+    },
+    {
+      Declaration: "Tax Saving Allowance",
+      NoOfDeclaration: 0,
+      AmountDeclared: 0,
+      ProofSUbmitted: 0,
+      AmountRejected: 0,
+      AmountAccepted: 0
+    },
+    {
+      Declaration: "House Property",
+      NoOfDeclaration: 0,
+      AmountDeclared: 0,
+      ProofSUbmitted: 0,
+      AmountRejected: 0,
+      AmountAccepted: 0
+    },
+    {
+      Declaration: "Income From Other Sources",
+      NoOfDeclaration: 0,
+      AmountDeclared: 0,
+      ProofSUbmitted: 0,
+      AmountRejected: 0,
+      AmountAccepted: 0
+    });
+
 
     this.monthlyTaxAmount = {
       april: 37050,
@@ -166,6 +165,44 @@ export class DeclarationComponent implements OnInit, AfterViewChecked {
       feb: 37050,
       march: 37050
     };
+  }
+
+  ngAfterViewChecked(): void {
+    $('[data-bs-toggle = "tooltip"]').tooltip();
+  }
+
+  filterDeduction() {
+    let value = this.filterValue.toLocaleUpperCase();
+  }
+
+  resetFilter() {
+    this.filterValue = '';
+
+    this.ExemptionDeclaration = this.allComponentDetails.ExemptionDeclaration;
+    this.OtherDeclaration = this.allComponentDetails.OtherDeclaration;
+    this.TaxSavingAlloance = this.allComponentDetails.TaxSavingAlloance;
+  }
+
+  getDeclaration(id: any) {
+    this.EmployeeId = id;
+    this.SectionIsReady = false;
+    this.http.get(`Declaration/GetEmployeeDeclarationDetailById/${this.EmployeeId}`).then((response:ResponseModel) => {
+      if (response.ResponseBody) {
+        if(response.ResponseBody.SalaryComponentItems && response.ResponseBody.SalaryComponentItems.length > 0) {
+          this.allComponentDetails = response.ResponseBody;
+          this.resetFilter();
+          this.EmployeeDeclarationId = response.ResponseBody.EmployeeDeclarationId;
+          this.employeeEmail = response.ResponseBody.Email;
+        }
+
+        if (response.ResponseBody && response.ResponseBody.FileDetails)
+          this.declarationFiles = response.ResponseBody.FileDetails;
+
+        Toast("Declaration detail loaded successfully");
+      }
+
+      this.SectionIsReady = true;
+    })
   }
 
   rentedResidence() {
@@ -379,20 +416,15 @@ export class DeclarationComponent implements OnInit, AfterViewChecked {
         formData.append('fileDetail', JSON.stringify(this.FileDocumentList));
         this.http.upload(`Declaration/UpdateDeclarationDetail/${this.EmployeeDeclarationId}`, formData).then((response: ResponseModel) => {
           if (response.ResponseBody) {
-            if(response.ResponseBody.SalaryComponentItems && response.ResponseBody.SalaryComponentItems.length > 0) {
-              this.employeeDeclaration = response.ResponseBody;
+            if(response.ResponseBody.length > 0) {
+              this.allComponentDetails = response.ResponseBody;
               this.resetFilter();
-              this.EmployeeDeclarationId = response.ResponseBody.EmployeeDeclarationId;
-              this.employeeEmail = response.ResponseBody.Email;
             }
 
-            if (response.ResponseBody && response.ResponseBody.FileDetails)
-              this.declarationFiles = response.ResponseBody.FileDetails;
-
-            Toast("Declaration detail updated successfully");
+            this.closeDeclaration(e);
+            Toast("Declaration Uploaded Successfully.");
+            this.SectionIsReady = true;
           }
-
-          this.SectionIsReady = true;
         });
       }
     } else {
@@ -517,4 +549,13 @@ class TaxAmount {
   TotalTaxPayable: number = 0;
   TaxAlreadyPaid: number = 0;
   RemainingTaxAMount: number = 0;
+}
+
+class MyDeclaration {
+  Declaration: string = '';
+  NoOfDeclaration: number = 0;
+  AmountDeclared: number = 0;
+  ProofSUbmitted: number = 0;
+  AmountRejected: number = 0;
+  AmountAccepted: number = 0;
 }
