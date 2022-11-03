91bcad9062899b47c6f7901846d075d702ffd662
istiyakmi9 <istiyaq.mi9@gmail.com>
2022-11-03 16:02:36 +0530

    added new module

---
+++ src/app/user-income-declaration/user-income-declaration.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserIncomeDeclarationRoutingModule } from './user-income-declaration-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UserIncomeDeclarationRoutingModule
+  ]
+})
+export class UserIncomeDeclarationModule { }
