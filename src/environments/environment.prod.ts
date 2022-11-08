3d207ffbd0a9dd750367b3b37146a46aa2ff70da
istiyakmi9 <istiyaq.mi9@gmail.com>
2022-11-08 13:57:23 +0530

    ointing to staging only

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  env: "production",
   OAuthClientId: "589159008359-r6hdfqsp2ukdc6gvkq618rk26c52i3p1.apps.googleusercontent.com",
   OAuthSecret: "RspdEibsTlVS0nKi4izhFu4I",
   baseUrl: "http://www.hiringbell.in:5000/api/",
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  env: "staging",
   OAuthClientId: "589159008359-r6hdfqsp2ukdc6gvkq618rk26c52i3p1.apps.googleusercontent.com",
   OAuthSecret: "RspdEibsTlVS0nKi4izhFu4I",
   baseUrl: "http://192.168.0.101:5000/api/",
