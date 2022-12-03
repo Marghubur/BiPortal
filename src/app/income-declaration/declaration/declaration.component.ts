07ace5dcf5730210bbf295b36250a4298a20a476
istiyakmi9 <istiyaq.mi9@gmail.com>
2022-12-03 22:43:51 +0530

    on delete no get call

--- src/app/income-declaration/declaration/declaration.component.ts
+++ src/app/income-declaration/declaration/declaration.component.ts
@@ -412,7 +412,7 @@ export class DeclarationComponent implements OnInit, AfterViewChecked {
           $('#addAttachmentModal').modal('hide');
           $('#housingPropertyFileModal').modal('hide');
           $('#rentedResidenceModal').modal('hide');
-          this.bindData(res.ResponseBody);
+          // this.bindData(res.ResponseBody);
           Toast("Declaration file is deleted successfully");
         }
         this.isLoading = false;
