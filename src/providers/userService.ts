35c54e258e03b259b17833216f13c2391ad4a381
istiyakmi9 <istiyaq.mi9@gmail.com>
2023-02-09 18:00:14 +0530

    ui chagnes

--- src/providers/userService.ts
+++ src/providers/userService.ts
@@ -18,31 +18,32 @@ export class UserService {
 }
 
 export class Filter {
-    SearchString: string = "1=1";
-    PageIndex: number = 1;
-    StartIndex: number = 0;
-    EndIndex: number = 0;
-    PageSize: number = 10;
-    SortBy: string = "";
-    CompanyId: number = 0;
-    TotalRecords: number = 0;
-    ShowPageNo: number = 5;
-    ActivePageNumber: number = 1;
-    isReUseSame: boolean = false;
-    isActive?: boolean = true;
+  EmployeeId: number = 0;
+  SearchString: string = "1=1";
+  PageIndex: number = 1;
+  StartIndex: number = 0;
+  EndIndex: number = 0;
+  PageSize: number = 10;
+  SortBy: string = "";
+  CompanyId: number = 0;
+  TotalRecords: number = 0;
+  ShowPageNo: number = 5;
+  ActivePageNumber: number = 1;
+  isReUseSame: boolean = false;
+  isActive?: boolean = true;
 
-    update(total: any) {
-      if(!isNaN(Number(total))) {
-        this.TotalRecords = total;
-        this.StartIndex = 1;
-        this.PageIndex = 1;
-      }
-    }
-
-    reset() {
-      this.TotalRecords = 0;
+  update(total: any) {
+    if(!isNaN(Number(total))) {
+      this.TotalRecords = total;
       this.StartIndex = 1;
       this.PageIndex = 1;
-      this.ActivePageNumber = 1;
+    }
   }
+
+  reset() {
+    this.TotalRecords = 0;
+    this.StartIndex = 1;
+    this.PageIndex = 1;
+    this.ActivePageNumber = 1;
+}
 }
