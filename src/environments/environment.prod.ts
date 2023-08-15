4f4232d6f49fd4f13e5c6aaeb8528bd77c61d588
md.istiyak <md.istiyak@primesoftinc.com>
2023-08-15 11:49:44 +0530

    route changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   OAuthClientId: "589159008359-r6hdfqsp2ukdc6gvkq618rk26c52i3p1.apps.googleusercontent.com",
   OAuthSecret: "RspdEibsTlVS0nKi4izhFu4I",
 
-  baseDotNetUrl: "http://www.hiringbell.in/dn/api/",
+  baseDotNetUrl: "http://www.hiringbell.in/dn/api/core/",
   baseSpringUrl: "http://www.hiringbell.in/sb/api/",
   
   FolderDelimiter: "\/"
