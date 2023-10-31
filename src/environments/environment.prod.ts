56a16639d3c2432eba21939a3cc5a68310472646
md.istiyak <md.istiyak@primesoftinc.com>
2023-10-31 12:40:11 +0530

    url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
   OAuthClientId: "589159008359-r6hdfqsp2ukdc6gvkq618rk26c52i3p1.apps.googleusercontent.com",
   OAuthSecret: "RspdEibsTlVS0nKi4izhFu4I",
 
-  baseDotNetUrl: "http://www.hiringbell.in/dn/core/api/",
-  baseSpringUrl: "http://www.hiringbell.in/sb/api/",
+  baseDotNetUrl: "http://www.emstum.com/dn/core/api/",
+  baseSpringUrl: "http://www.emstum.com/sb/api/",
 
   FolderDelimiter: "\/"
 };
