f25dffb006884ae9c3ed776a969332c2ae71ebf3
md.istiyak <md.istiyak@primesoftinc.com>
2023-11-19 21:21:28 +0530

    url changed

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -4,8 +4,8 @@ export const environment = {
   OAuthClientId: "589159008359-r6hdfqsp2ukdc6gvkq618rk26c52i3p1.apps.googleusercontent.com",
   OAuthSecret: "RspdEibsTlVS0nKi4izhFu4I",
 
-  baseDotNetUrl: "http://bot.io/dn/api/",
-  baseSpringUrl: "http://bot.io/sb/api/eps/",
+  baseDotNetUrl: "http://bot.io/bot/dn/core/api/",
+  baseSpringUrl: "http://bot.io/bot/sb/api/eps/",
 
   FolderDelimiter: "\/"
 };
