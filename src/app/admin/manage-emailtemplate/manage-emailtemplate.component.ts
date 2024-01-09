bc8aa2b58c497cc24f075e431038ecf7efa6bc81
Marghub <marghub12@gmail.com>
2024-01-09 19:54:59 +0530

    Issue fixed

--- src/app/admin/manage-emailtemplate/manage-emailtemplate.component.ts
+++ src/app/admin/manage-emailtemplate/manage-emailtemplate.component.ts
@@ -134,7 +134,7 @@ export class ManageEmailtemplateComponent implements OnInit {
     this.submitted = true;
     //let data = document.getElementById("content-container").innerHTML;
     let data = (document.getElementById("richTextField") as HTMLIFrameElement).contentWindow.document.body.innerHTML;
-    if (this.emailTemplateForm.invalid || (data && data == "")) {
+    if (this.emailTemplateForm.invalid || !data || data == "") {
       this.isLoading = false;
       return;
     }
@@ -142,7 +142,7 @@ export class ManageEmailtemplateComponent implements OnInit {
     let value = this.emailTemplateForm.value;
     value.BodyContent = data;
     formData.append('emailtemplate', JSON.stringify(value));
-    this.http.post("Email/InsertUpdateEmailTemplate", formData).then((res:ResponseModel) => {
+    this.http.post("Email/InsertUpdateEmailTemplate", formData, false).then((res:ResponseModel) => {
       if (res.ResponseBody && res.ResponseBody != '') {
         this.emailTemplateId = Number(res.ResponseBody);
         this.emailTemplateForm.get('EmailTemplateId').setValue(this.emailTemplateId);
