592a10419ad25f92d087698477517a7f5f5800e1
BottomHalf <istiyaq.mi9@gmail.com>
2024-05-10 20:45:23 +0530

    # removed from routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,29 +9,30 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    loadChildren: () => import('./home/home.module')
-    .then(m => m.HomeModule)
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
   {
     matcher: (url) => {
-      if(url[0].path.split(/\/(.*)/s)[0] == 'bot') {
+      if (url[0].path.split(/\/(.*)/s)[0] == 'bot') {
         return {
-          consumed: url
+          consumed: url,
         };
       }
       return null;
     },
     path: '',
     component: LayoutComponent,
-    loadChildren: () => import('./layout/layout.module')
-    .then(m => m.LayoutModule)
-  }, {
-    path: RegisterNewOrg, component: RegisterNewOrgComponent
-  }
+    loadChildren: () =>
+      import('./layout/layout.module').then((m) => m.LayoutModule),
+  },
+  {
+    path: RegisterNewOrg,
+    component: RegisterNewOrgComponent,
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
